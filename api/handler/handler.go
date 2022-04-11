6977bdc2c79265d7219c88895a0ee657647cad95
ishank <ishankbansal838@gmail.com>
2022-04-11 13:08:42 +0530

    added messages to handlers

--- api/handler/handler.go
+++ api/handler/handler.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 
 	"github.com/ishank838/data-collection/models"
@@ -21,11 +20,27 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 
 	paresedReq := request.ParseInsertRequest()
 
+	if paresedReq.Country == "" || paresedReq.Device == "" || paresedReq.TimeSpent == 0 || paresedReq.WebRequest == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		msg := map[string]string{
+			"status": http.StatusText(http.StatusBadRequest),
+			"error":  "incomplete request",
+		}
+		m, _ := json.Marshal(msg)
+		w.Write(m)
+		return
+	}
+
 	t := datatree.GetTree()
 
-	go t.Insert(paresedReq)
+	t.Insert(paresedReq)
 
 	w.WriteHeader(http.StatusOK)
+	msg := map[string]string{
+		"status": http.StatusText(http.StatusOK),
+	}
+	m, _ := json.Marshal(msg)
+	w.Write(m)
 }
 
 func Query(w http.ResponseWriter, r *http.Request) {
@@ -35,6 +50,12 @@ func Query(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		msg := map[string]string{
+			"status": http.StatusText(http.StatusBadRequest),
+			"error":  "invalid request",
+		}
+		m, _ := json.Marshal(msg)
+		w.Write(m)
 		return
 	}
 
@@ -45,12 +66,17 @@ func Query(w http.ResponseWriter, r *http.Request) {
 	resp, err := t.Query(&parsedReq)
 
 	if err != nil {
-		log.Println(err)
+		//log.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
+		msg := map[string]string{
+			"status": http.StatusText(http.StatusBadRequest),
+			"error":  "required country or devices doesn't exist",
+		}
+		m, _ := json.Marshal(msg)
+		w.Write(m)
 		return
 	}
 
-	//type,name := request.GetQueryName()
 	respJson, _ := json.Marshal(resp)
 	w.Write(respJson)
 }
